f5dedfce3e88a7e62eb1c7627917e958f7aa2456
Marcelino <marce93p@gmail.com>
2019-11-26 16:59:23 +0000

    new feature and refactoring
    feat: ctrlKey and metaKey supported to open link in new tab
    refactor: remove a function to use just one.

--- no-youtube.js
+++ no-youtube.js
@@ -10,30 +10,30 @@
 var j = jQuery.noConflict(true);
 
 j(document).ready(function ($) {
- 	 $('a').click(function(event){
-     var addressValue = $(this).attr("href");
-     var target = $(this).attr("target");
-     console.log("target: " + target);
-     if(addressValue.includes("youtube.com/")) {
-       event.preventDefault();
-       generateUrl(addressValue, target);
-     } else if (addressValue.includes("youtu.be/")) {
-  			event.preventDefault();
-       	generateUrlAlternative(addressValue, target);
-     }
-	});
+  var longURL = "youtube.com/";
+  var shortURL = "youtu.be/";
+
+  $('a').click(function (event) {
+    var addressValue = $(this).attr("href");
+    var target = $(this).attr("target");
+    console.log("target: " + target);
+    if (event.ctrlKey || event.metaKey) {
+      target = "_blank";
+    }
+
+    if (addressValue.includes(longURL)) {
+      event.preventDefault();
+      generateUrl(addressValue, target, longURL);
+    } else if (addressValue.includes(shortURL)) {
+      event.preventDefault();
+      generateUrl(addressValue, target, shortURL);
+    }
+  });
 });
 
-function generateUrl(addressValue, target) {
-  var splitstring = addressValue.split("youtube.com/");
-  var url = "https://invidio.us/" + splitstring[splitstring.length -1];
-  console.log("url: " + url);
-  window.open(url, target);
-}
-
-function generateUrlAlternative(addressValue, target) {
-  var splitstring = addressValue.split("youtu.be/");
-  var url = "https://invidio.us/" + splitstring[splitstring.length -1];
+function generateUrl(addressValue, target, ytURL) {
+  var splitstring = addressValue.split(ytURL);
+  var url = "https://invidio.us/" + splitstring[splitstring.length - 1];
   console.log("url: " + url);
   window.open(url, target);
 }
