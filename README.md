877ed07ebf87e2940e8154aec8d5d362ca8df22f
Marcelino <marce93p@gmail.com>
2019-11-26 09:35:16 +0000

    feat: functional script and read.me updated
    Script working and read.me updated with info about the script and its use

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# no-youtube
-Script to replace youtube links to invisio.us
+# No Youtube script
+no-youtube is a script to replace youtube links to invidio.us
+
+## About
+This script exists with the purpose of avoiding the use of Google's services, but still being able to see videos hosted on YouTube from another platform with greater privacy.
+To facilitate this, it has created a script that prevents access to the links to YouTube, opening the content on the web invidio.us
+
+## Installation
+To use this script it is necessary to have an userscript administrator.
+If you use Mozilla Firefox, you can use [Greasemonkey](https://addons.mozilla.org/firefox/addon/greasemonkey/) addon. 
+
+If you use Google Chrome, you can use [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl).
+
+Once we have the script manager, simply copy and paste the .js to a new user script in the addon.  
---
+++ no-youtube.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name     no-youtube
+// @description		Replace youtube links to invisio.us 
+// @version  1.0.1
+// @grant    none
+// @require        https://code.jquery.com/jquery-2.2.2.js
+// @run-at        document-end
+// ==/UserScript==
+
+var j = jQuery.noConflict(true);
+
+j(document).ready(function ($) {
+ 	 $('a').click(function(event){
+     var addressValue = $(this).attr("href");
+     var target = $(this).attr("target");
+     console.log("target: " + target);
+     if(addressValue.includes("youtube.com/")) {
+       event.preventDefault();
+       generateUrl(addressValue, target);
+     } else if (addressValue.includes("youtu.be/")) {
+  			event.preventDefault();
+       	generateUrlAlternative(addressValue, target);
+     }
+	});
+});
+
+function generateUrl(addressValue, target) {
+  var splitstring = addressValue.split("youtube.com/");
+  var url = "https://invidio.us/" + splitstring[splitstring.length -1];
+  location.href=url;
+  window.open(url, target);
+}
+
+function generateUrlAlternative(addressValue, target) {
+  var splitstring = addressValue.split("youtu.be/");
+  var url = "https://invidio.us/" + splitstring[splitstring.length -1];
+  console.log("url: " + url);
+  window.open(url, target);
+}
